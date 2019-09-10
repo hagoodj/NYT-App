15b3a65bc869a3e85e36330a6e6d19c43e7c22c0
Kerwin Hy <kerwin.hy@gmail.com>
2019-09-10 14:05:06 -0700

    functions added for search and clear functions

--- assets/javascript/derp.js
+++ assets/javascript/derp.js
@@ -1,32 +1,42 @@
-
-
-
-
-// article search url: https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=ubVFwmgMiXClcqzz9FyoX639MSKR6wGt
-// start/end year url: q=obama&facet_fields=source&facet=true&begin_date=20120101&end_date=20121231
-
-// ajax function to pull info from nytimes api
-var q = $("#search-term").val();
-var startDate = $("#start-year").val();
-var endDate = $("#end-year").val();
-var resultsNum = $("records-retrived option:selected").val();
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
-    q + "&start_date=" + startDate + "0101&end_date=" + endDate + "1231&api-key=ubVFwmgMiXClcqzz9FyoX639MSKR6wGt"
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    var results = response.response.docs;
-    console.log(response);
-})
-
 // when search button is clicked
 $("#search").on("click", function() {
+    event.preventDefault();
+
+    var q = $("#search-term").val();
+    var startDate = $("#start-year").val();
+    var endDate = $("#end-year").val();
+    var resultsNum = $("records-retrived option:selected").val();
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
+    q + "&start_date=" + startDate + "0101&end_date=" + endDate + "1231&api-key=ubVFwmgMiXClcqzz9FyoX639MSKR6wGt"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        var results = response.response.docs;
+        for (var i = 0; i < resultsNum; i ++) {
+            var article = ("<div>");
+            var headline = " " + results[i].headline.print_headline;
+            var byLine = " " + results[i].byline.original;
+            var section = "<br>Section: " + results[i].section_name;
+            var pubDate = "<br>" + results[i].pub_date;
+            var pp = results[i].print_page +  " pp.";
+            var articleUrl = "<br>" + results[i].web_url;
+
+            article.append(i, headline, byLine, pp, section, pubDate, articleUrl)
+            $("#articles-view").append(article);
+        }
+    })
 
 })
 
 // when clear button is clicked
 $("#clear").on("click", function() {
-    
+    event.preventDefault();
+
+    // reset the input texts
+    $("#search-term").val("");
+    $("#records-retrieved").val("");
+    $("#start-year").val("");
+    $("#end-year").val("");
 })
